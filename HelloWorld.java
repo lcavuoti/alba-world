4f4d0e69b6fd9502e0812d8e8e81eb80d4b5e98b
Cavuoti Luigi <LP5LCavuoti@gibwintra.ch>
2019-11-19 08:19:30 +0100

    get und set added

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+	
+	private String hello;
+	
+	
+	public String getHello() {
+		return this.hello;
+	}
+	
+	public void setHello(String hello) {
+			this.hello = hello;
+	}	
+	
+}
